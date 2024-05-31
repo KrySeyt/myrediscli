b0eda1543d16adfea031798d6167457831d173b0
Vladimir Kibisov <kryseyt@gmail.com>
2024-05-31 15:17:41 +0300

    rename

--- main/mocked.fs
+++ main/mocked.fs
@@ -1,6 +1,8 @@
+namespace MyRedisCLI.main
+
 open MyRedisCLI.external
 open MyRedisCLI.application
 
-module main =
+module MockedMain =
     let value = Interactors.get MockRedis.get "Test"
     printfn "%s" value
